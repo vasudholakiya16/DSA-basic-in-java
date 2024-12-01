056ac900b8f0c1f68605a6cf2884917ce358ec93
vasudholakiya16 <122475789+vasudholakiya16@users.noreply.github.com>
2024-12-01 14:21:27 +0530

    Add new pattern printing methods and update main execution flow

--- src/PatternQuestion/patternQuestion.java
+++ src/PatternQuestion/patternQuestion.java
@@ -109,7 +109,62 @@ public class patternQuestion {
     }
 
     static void pattern30(int n ){
+        for (int row = 0; row <=n ; row++) {
 
+            for (int spaces = 0; spaces <n-row ; spaces++) {
+                System.out.print("  ");
+            }
+            
+            for (int column = row; column >=1 ; column--) {
+                System.out.print(column + " ");
+                
+            }
+            for (int column = 2 ; column <=row ; column++) {
+                System.out.print(column+ " ");
+                
+            }
+            System.out.println();
+
+        }
+
+
+    }
+
+    static void pattern17(int n ){
+        for (int row = 1; row <=2 * n ; row++) {
+            int c = row > n ?  2 * n -row : row;
+
+
+            for (int spaces = 0; spaces <n-c ; spaces++) {
+                System.out.print("  ");
+            }
+
+            for (int column = c; column >=1 ; column--) {
+                System.out.print(column + " ");
+
+            }
+            for (int column = 2 ; column <=c ; column++) {
+                System.out.print(column+ " ");
+
+            }
+            System.out.println();
+
+        }
+
+
+    }
+
+    static  void  pattern31(int n ){
+        n = 2*n;
+        for (int row = 0; row <n  ; row++) {
+            for (int column = 0; column <n ; column++) {
+
+                int ateveryIndex = Math.min(Math.min(row,column),Math.min(n-row, n-column));
+                System.out.print(ateveryIndex + " ");
+            }
+            System.out.println();
+            
+        }
     }
 
 
@@ -126,6 +181,12 @@ public class patternQuestion {
         pattern5(4);
         System.out.println("Pattern 28 Answer");
         pattern28(4);
+        System.out.println("Pattern 30 Answer");
+        pattern30(4);
+        System.out.println("Pattern 17 Answer");
+        pattern17(4);
+        System.out.println("Pattern 31 Answer");
+        pattern31(4);
 
 
     }
