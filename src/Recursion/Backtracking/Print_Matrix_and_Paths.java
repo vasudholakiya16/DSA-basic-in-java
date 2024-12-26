38084dc7fe4c0e3490d2dc7f49b29bc2c2ac9cf3
vasudholakiya16 <122475789+vasudholakiya16@users.noreply.github.com>
2024-12-26 09:08:12 +0530

    Add Maze_Get_AllPath implementation and Print_Matrix_and_Paths class; update workspace configurations

--- src/Recursion/Backtracking/Print_Matrix_and_Paths.java
+++ src/Recursion/Backtracking/Print_Matrix_and_Paths.java
@@ -1,12 +1,19 @@
-
 package Recursion.Backtracking;
 
-public class Maze_Get_AllPath {
+import java.util.Arrays;
 
-    static void mazeAllDirection(String process, boolean[][] maze, int row, int column) {
+public class Print_Matrix_and_Paths {
+
+    static void mazeAllDirection(String process, boolean[][] maze, int row, int column,int [] [] path
+    ,int step) {
         // Base case: If we reach the bottom-right corner, print the path
         if (row == maze.length - 1 && column == maze[0].length - 1) {
+            path[row][column] = step;
+            for (int[] arr:path ) {
+                System.out.println(Arrays.toString(arr));
+            }
             System.out.println(process);
+            System.out.println();
             return;
         }
 
@@ -17,22 +24,23 @@ public class Maze_Get_AllPath {
 
         // Mark the current cell as visited
         maze[row][column] = false;
+        path[row][column]= step;
 
         // Move Down
         if (row < maze.length - 1) {
-            mazeAllDirection(process + 'D', maze, row + 1, column);
+            mazeAllDirection(process + 'D', maze, row + 1, column,path,step+1);
         }
         // Move Right
         if (column < maze[0].length - 1) {
-            mazeAllDirection(process + 'R', maze, row, column + 1);
+            mazeAllDirection(process + 'R', maze, row, column + 1,path,step+1);
         }
         // Move Up
         if (row > 0) {
-            mazeAllDirection(process + 'U', maze, row - 1, column);
+            mazeAllDirection(process + 'U', maze, row - 1, column,path,step+1);
         }
         // Move Left
         if (column > 0) {
-            mazeAllDirection(process + 'L', maze, row, column - 1);
+            mazeAllDirection(process + 'L', maze, row, column - 1,path,step+1);
         }
 
         // Backtrack: Unmark the current cell as visited
@@ -40,7 +48,9 @@ public class Maze_Get_AllPath {
 //        // so before the function gets remove, also remove the changes
 //        // that where made by this function:- this process is known as back tracking
         maze[row][column] = true;
+        path[row][column]=0;
     }
+
 
     public static void main(String[] args) {
         boolean[][] board = {
@@ -48,6 +58,8 @@ public class Maze_Get_AllPath {
                 {true, true, true},
                 {true, true, true},
         };
-        mazeAllDirection("", board, 0, 0);
+        int[][] path = new int[board.length][board[0].length];
+        mazeAllDirection("", board, 0, 0,path,1);
+
     }
 }
